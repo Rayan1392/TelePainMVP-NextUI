e48cfd2fafbdda1d0ded35e0bb7dc9f4134b72d2
saman <samanmgh@gmail.com>
2025-02-08 14:53:27 +0330

    handle mini header

--- src/components/app/header.tsx
+++ src/components/app/header.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 import Link from "next/link";
 import {useLocale} from "next-intl";
 // import {useRouter} from "next/navigation";
-import logo from '@/assets/images/logo.png'
+import Logo from "../../assets/images/logo.png"
 import {PlanetIcon} from "@/assets/icons";
 
 export default function Header() {
@@ -16,11 +16,11 @@ export default function Header() {
 
   return (
     <header className="fixed top-0 right-0 w-full h-[98px] z-20 shadow-custom-header bg-white">
-      <div className="px-[56px] h-full">
+      <div className="px-4 md:px-[56px] h-full">
         <div className="flex justify-between h-full items-center">
           <Link href='/'>
             <Image
-              src={logo}
+              src={Logo}
               width={55}
               height={50}
               fill={false}
